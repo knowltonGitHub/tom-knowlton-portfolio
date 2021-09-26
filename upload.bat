60a80e2b851d4554e79b6ea7da8a4bf3031ce6c9
softwaredevr <softwaredevr@gmail.com>
2021-09-26 14:02:12 -0600

    pig first to get to 100 wins

---
+++ upload.bat
@@ -0,0 +1,7 @@
+echo "# udemy-javascript-pig" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/knowltonGitHub/udemy-javascript-pig.git
+git push -u origin main
