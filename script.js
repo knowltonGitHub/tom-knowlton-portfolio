5bc12462433741533d3b1ed45d47f1f4e4c2eb40
Tom <softwaredevr@gmail.com>
2024-05-08 14:17:41 -0600

    s

--- script.js
+++ script.js
@@ -109,15 +109,15 @@ btnNew.addEventListener('click', function () {
 
 $(document).ready(function() {
   // Trigger modal on link click (replace 'https://example.com' with the actual URL)
-  $('.technology-link').click(function(e) {
+  $('a.technology-link').click(function(e) {
     e.preventDefault(); // Prevent default link behavior (opening in new window)
     var url = $(this).attr('href'); // Get the URL from the link
-
+alert(url);
     // Use jQuery's AJAX to fetch content
     $.ajax({
       url: url,
       success: function(data) {
-        alert(data);
+        
         // Once data is fetched, update the modal content
         $('#modalContent').html(data);
         // Show the modal (replace with your modal show function)
