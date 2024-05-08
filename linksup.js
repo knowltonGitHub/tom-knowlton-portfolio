d7f6f8f153a0037a6b1431006dc8dd37906ac832
Tom <softwaredevr@gmail.com>
2024-05-08 14:24:06 -0600

    s

---
+++ linksup.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    // Trigger modal on link click (replace 'https://example.com' with the actual URL)
+    $('a.technology-link').click(function(e) {
+      e.preventDefault(); // Prevent default link behavior (opening in new window)
+      var url = $(this).attr('href'); // Get the URL from the link
+  alert(url);
+      // Use jQuery's AJAX to fetch content
+      $.ajax({
+        url: url,
+        success: function(data) {
+          
+          // Once data is fetched, update the modal content
+          $('#modalContent').html(data);
+          // Show the modal (replace with your modal show function)
+          $('#myModal').modal('show');
+        }
+      });
+    });
+  });
+  
--- script.js
+++ script.js
@@ -107,22 +107,3 @@ btnNew.addEventListener('click', function () {
 
 
 
-$(document).ready(function() {
-  // Trigger modal on link click (replace 'https://example.com' with the actual URL)
-  $('a.technology-link').click(function(e) {
-    e.preventDefault(); // Prevent default link behavior (opening in new window)
-    var url = $(this).attr('href'); // Get the URL from the link
-alert(url);
-    // Use jQuery's AJAX to fetch content
-    $.ajax({
-      url: url,
-      success: function(data) {
-        
-        // Once data is fetched, update the modal content
-        $('#modalContent').html(data);
-        // Show the modal (replace with your modal show function)
-        $('#myModal').modal('show');
-      }
-    });
-  });
-});
