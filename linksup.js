f05ec688ff8541d1ffc0b183d8f1e70179e9afc9
Tom <softwaredevr@gmail.com>
2024-05-08 14:33:26 -0600

    s

--- linksup.js
+++ linksup.js
@@ -3,12 +3,13 @@ $(document).ready(function() {
     $('a.technology-link').click(function(e) {
       e.preventDefault(); // Prevent default link behavior (opening in new window)
       var url = $(this).attr('href'); // Get the URL from the link
-  alert(url);
+      alert(url);
       // Use jQuery's AJAX to fetch content
       $.ajax({
         url: url,
+        dataType: 'jsonp', // Specify JSONP data type
         success: function(data) {
-          
+          alert(data);
           // Once data is fetched, update the modal content
           $('#modalContent').html(data);
           // Show the modal (replace with your modal show function)
