38b4b7bda57676c81f3eaa381478d8d2dc8f4984
Tom <softwaredevr@gmail.com>
2024-06-25 14:57:13 -0600

    s

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+$(".clickable-div").click(function() {
+    // Get the URL from the nested anchor tag
+    var url = $(this).find("a").attr("href");
+
+    // Prevent default behavior (following the link normally)
+    event.preventDefault();
+
+    // Redirect to the URL using window.location.href
+    window.location.href = url;
+  });
