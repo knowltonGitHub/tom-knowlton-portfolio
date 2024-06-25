8c3435ff44bfb544af6fb937cde8bdc51fba8e32
Tom <softwaredevr@gmail.com>
2024-06-25 16:48:04 -0600

    s

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
+$(document).ready(function() {
 $(".clickable-div").click(function() {
+
     // Get the URL from the nested anchor tag
     var url = $(this).find("a").attr("href");
 
@@ -10,3 +12,13 @@ $(".clickable-div").click(function() {
     // Redirect to the URL using window.location.href
     window.location.href = url;
   });
+
+  $(".clickable-div").hover(function() {
+    // Change background color on hover
+    $(this).css("background-color", "red");
+  }, function() {
+    // Change background color back to original on mouseout (optional)
+    $(this).css("background-color", "#777777"); // Or remove this line to keep the hover color
+  });
+
+});
