9b841f5590254adef8ba57fc2be93519688ab2de
Tom <softwaredevr@gmail.com>
2024-08-13 15:32:43 -0600

    rb

--- portfolio/assets/js/site.js
+++ portfolio/assets/js/site.js
@@ -1,6 +1,9 @@
 function buildEmailLink() {
+    console.log("emailing");
     var subject = document.getElementById("subject").value;
     var body = document.getElementById("message").value;
+
+    console.log("emailing" + subject + body);
     var mailtoLink = "mailto:softwaredevr@gmail.com?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
     window.location.href = mailtoLink;
   }
