555a0398fa4b5be59b4b21a3b2a8a01ae2ad87c0
Tom <softwaredevr@gmail.com>
2024-08-26 13:16:00 -0600

    vc

--- portfolio/assets/js/visitor_count.js
+++ portfolio/assets/js/visitor_count.js
@@ -14,11 +14,12 @@ const urlParams = new URLSearchParams(window.location.search);
 const param1Value = urlParams.get('param1');
 const param2Value = urlParams.get('param2');
 
-console.log("param1:", param1Value);  // Output: param1: 1
+//console.log("param1:", param1Value);  // Output: param1: 1
 //console.log("param2:", param2Value);  // Output: param2: horse
 
 if(param1Value === "cow")
 {
+    console.log("param1:", param1Value);
     //reset visitor count to 0
     const visitorCountElement = document.getElementById('visitor-count');
     visitorCountElement.textContent = 0;
